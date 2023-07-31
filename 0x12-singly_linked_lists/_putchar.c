1597b96da9360a1e552ac6ca3da79d4a58eb4872
Kufre Akpan <contactkufreakpan@gmail.com>
2023-07-31 04:50:02 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+  * putchar - writes character c to stdout
+  * @C: the character to print
+  *
+Return: on success 1.
+On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
