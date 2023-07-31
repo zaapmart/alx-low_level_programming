db32f8e05bba60dfcff8e882e78d21a32ed4d931
Kufre Akpan <contactkufreakpan@gmail.com>
2023-07-31 06:17:32 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * free_list - Release the memory allocated for a list.
+  *
+  * head: A pointer to the first node of the list to free.
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
