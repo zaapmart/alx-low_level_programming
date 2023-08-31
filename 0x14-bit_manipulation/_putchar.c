5ea1eb9907daf3b5995c45554f57f03dfcfb8b6b
Kufre Akpan <contactkufreakpan@gmail.com>
2023-08-31 14:44:04 -0700

    recommit file

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to print
+ *
+ * Return: Return 1 On success.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
