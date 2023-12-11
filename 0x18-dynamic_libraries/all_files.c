898d11e4ce106735353c13130761104ac4b5ccf1
Kufre Akpan <contactkufreakpan@gmail.com>
2023-12-11 13:25:03 -0800

    import from c to python

---
+++ 0x18-dynamic_libraries/all_files.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - method to sum up two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return the value from the addition
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - method to subtract one integer from another
+ * @a: first integer
+ * @b: second integer
+ * Return: return the value from the subtraction
+ *
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - method to multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return the value from the multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - method to divide one integer with another
+ * @a: first integer
+ * @b: second integer
+ * Return: return the value from the division
+ *
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - method to find a modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return the value of the modulus
+ *
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
